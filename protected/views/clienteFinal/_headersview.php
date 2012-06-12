f11841b7235919d07a76038b76bc9b3c7980095a
Roberto Zamarripa <rzamarripa@uss.mx>
2012-06-12 10:28:37 -0600

    vistas videos

---
+++ protected/views/clienteFinal/_headersview.php
@@ -0,0 +1,6 @@
+<table>
+	<tr>
+		<td>
+			<?php echo CHtml::encode($data->nombre); ?>
+		</td>
+	</tr>
---
+++ protected/views/clienteFinal/_view.php
@@ -0,0 +1,5 @@
+	<tr>
+		<td>
+			<?php echo CHtml::encode($data->url), array('view', 'id'=>$data->id)); ?>
+		</td>
+	</tr>
