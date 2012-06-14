f6da7525bd38c027b14afa53ef900ab0331abd9a
Roberto Zamarripa <rzamarripa@uss.mx>
2012-06-14 13:04:07 -0600

    cliente

--- protected/views/video/_view.php
+++ protected/views/video/_view.php
@@ -6,8 +6,10 @@
 			<?php echo CHtml::encode($data->nombre); ?>
 		</td>
 		<td>
-			<video width="320" height="250" controls="controls">
-			  <source src="../../../videos/recursos/<?php echo CHtml::encode($data->url); ?>" type="video/mp4" />
+			<video id="example_video_1" class="video-js vjs-default-skin" controls preload="auto" width="350" height="264" poster="" data-setup=''>  
+				<source src="../../../videos/recursos/<?php echo CHtml::encode($data->url); ?>" type='video/mp4' />  
+				<source src="../../../videos/recursos/<?php echo CHtml::encode($data->url); ?>" type='video/ogg' />
+				<source src="../../../videos/recursos/<?php echo CHtml::encode($data->url); ?>" type='video/webm' /> 
 			</video>
 		</td>
 		<td>
